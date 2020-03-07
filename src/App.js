e778ecd2a0278eca8d8655850e04f2dedb7f2502
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-03-07 12:08:39 +0800

    Add more content and info

--- src/App.js
+++ src/App.js
@@ -6,8 +6,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Charmap />
+        <div className="logo">
+          <h1>
+            <span>Copy</span>
+            <span>Glyphs</span>
+          </h1>
+        </div>
+        <div className="desc">
+          Copy & paste special characters and symbols with ēase.
+        </div>
       </header>
+      <Charmap />
+      <footer className="App-footer">
+        <h2>About</h2>
+        <p>CopyGlyphs is a simple online tool that helps you easily copy any special characters and symbols (also known as glyphs) then paste them to any document or app that you are using.</p>
+        <h2>Using WordPress?</h2>
+        <p>Install and activate <a href="https://wordpress.org/plugins/block-options/">EditorsKit Plugin↗</a> which is available for free on plugin directory. EditorsKit adds set of formatting tools and helps improve writing workflow when using WordPress Gutenberg block editor.</p>
+        <p>Made by <a href="https://jeffreycarandang.com/">Jeffrey Carandang↗</a> - creator of EditorsKit plugin for WordPress.</p>
+      </footer>
     </div>
   );
 }
