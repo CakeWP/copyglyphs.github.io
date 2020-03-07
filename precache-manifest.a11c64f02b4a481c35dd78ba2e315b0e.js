bff20ad2562fb45507de5d3a8e863e897ac7ee8e
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-03-07 10:11:57 +0800

    Updates

---
+++ precache-manifest.a11c64f02b4a481c35dd78ba2e315b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5ca5ad8a5b4de79fde0fd727421431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc2f87a13f5c29c13e9",
+    "url": "/static/css/main.5c12f1a4.chunk.css"
+  },
+  {
+    "revision": "9f521da76cec42621103",
+    "url": "/static/js/2.5aa1fa3b.chunk.js"
+  },
+  {
+    "revision": "03fe4d632d4c060cb7cfad7727730c86",
+    "url": "/static/js/2.5aa1fa3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc2f87a13f5c29c13e9",
+    "url": "/static/js/main.dbbed952.chunk.js"
+  },
+  {
+    "revision": "a22687e33bd749c2a2cb",
+    "url": "/static/js/runtime-main.af449695.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
